a53ce649e909130e6b13d9a9cf65d2c1f6d7a3d0
3omdaughh <mohamade.hamdi14@gmail.com>
2025-07-23 20:01:06 +0300

    Almost finished the header files

--- include/ins.h
+++ include/ins.h
@@ -1,16 +1,19 @@
 #ifndef INS_H
 #define INS_H
 
+#include <stdlib.h> 
+#include <stddef.h> 
+
+/* Macro to append to a dynamic array (vec *xs) */
 #define da_append(xs, x)\
 	do \
 	{ \
-		if (xs.count >= xs.capacity) \
+		if ((xs)->count >= (xs)->capacity) \
 		{ \
-			if (xs.capacity == 0) xs.capacity = 256; \
-			else xs.capacity *= 2; \
-			xs.items = realloc (xs.items, xs.capacity*sizeof(*xs.items));\
+			(xs)->capacity = (xs)->capacity == 0 ? 256 : (xs)->capacity * 2; \
+			(xs)->items = realloc((xs)->items, (xs)->capacity * sizeof(*((xs)->items))); \
 		} \
-		xs.items[xs.count++] = x;\
+		(xs)->items[(xs)->count++] = (x); \
 	} while(0)
 
 typedef struct 
@@ -29,9 +32,9 @@ typedef struct
 	size_t capacity;
 } vec;
 
-void addItem(/* dynamaic array in C */ );
-void deleteItem(/* dynamaic array in C */ );
-void searchItem(/* dynamaic array in C */ );
-void generateReports(/* dynamaic array in C */ );
+void addItem(vec *v);
+void deleteItem(vec *v);
+void searchItem(const vec *v); /* const cuz it's read-only */
+void generateReports(const vec *v); /* const cuz it's read-only */
 
 #endif
